5f3063eec256c23c9411cc6cc5f8fb9ccf16f494
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2016-10-04 13:48:21 +0300

    Update Clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/MattiNieminen/tiko-example"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha13"]]
   :main ^:skip-aot tiko-example.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
